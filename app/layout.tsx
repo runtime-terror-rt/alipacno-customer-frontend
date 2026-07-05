690389eb72d50ecbfdde3ca5b8a530d2f6225c52
theashim-3 <ashim@softvence.com>
2026-07-05 13:34:19 +0600

    updated the home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,10 +34,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header />
         {children}
-        {/* Client-only modal provider - mounts on first render */}
-        <StartOrderClient />
       </body>
     </html>
   );
